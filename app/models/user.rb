035a7d453b7e55ba17a15c2f66e221702661dea7
hanacat299 <flower.catlnver63@gmail.com>
2024-06-02 18:31:45 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'must include both letters and numbers' }
-  validates :password, format: { without: /\p{Han}|\p{Hiragana}|\p{Katakana}/, message: 'cannot contain full-width characters' }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'must include both letters and numbers' }
   validates :sei, presence: true, format: { with: /\A[^\x20-\x7E]+\z/, message: "can't contain half-width characters" }
   validates :mei, presence: true, format: { with: /\A[^\x20-\x7E]+\z/, message: "can't contain half-width characters" }
   validates :kana_sei, presence: true, format: { with: /\A[\p{katakana}\u30A0-\u30FF]+\z/, message: "must be in katakana" }
