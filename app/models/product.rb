bedcd261af25b4ab11950a7f2e86d238dbc82907
hanacat299 <flower.catlnver63@gmail.com>
2024-06-04 23:08:49 +0900

    Activeの導入

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_03_152138) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_04_133641) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,18 +39,23 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_03_152138) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "articles", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", charset: "utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "product_name", null: false
     t.text "explanation", null: false
     t.integer "price", null: false
-    t.integer "genre_id", null: false
     t.integer "condition_id", null: false
     t.integer "shipping_cost_id", null: false
     t.integer "prefecture_id", null: false
     t.integer "shipping_day_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "genre_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
