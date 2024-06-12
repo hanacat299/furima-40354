c5b4c082fda5eb612fd8a9686550940b125db131
hanacat299 <flower.catlnver63@gmail.com>
2024-06-12 23:27:02 +0900

    削除機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
-  before_action :set_product, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
  
 
   def index
@@ -39,6 +39,11 @@ class ProductsController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to root_path
   end
 
 
