53c02260fbc16082b23776eacbfe09e976d1a20f
hanacat299 <flower.catlnver63@gmail.com>
2024-06-12 20:27:48 +0900

    コードの整理：重複するコードをまとめる

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :set_product, only: [:show, :edit, :update]
+ 
 
   def index
     @products = Product.order(created_at: :desc)
@@ -19,24 +21,19 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
-    if user_signed_in?
+
      if @product.user == current_user
       render "edit"
      else
       redirect_to root_path
      end
-    else
-     redirect_to new_user_session_path
-    end
+    
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to product_path(@product)
     else
@@ -52,6 +49,10 @@ class ProductsController < ApplicationController
       :product_name, :explanation, :price, :genre_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, :image
       )
       .merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 
 end
